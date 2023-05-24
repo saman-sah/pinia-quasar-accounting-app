b4a9e3bb88b732689aaa5b9ac5fcba51ce643791
SamanSah <sahraei.saman91@gmail.com>
2023-05-24 15:34:11 +0330

    ajax bar for async functions

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
+  <q-ajax-bar
+    ref="bar"
+    position="bottom"
+    color="accent"
+    size="10px"
+    skip-hijack
+  />
   <router-view />
 </template>
 
 <script setup>
-import { onBeforeMount } from 'vue';
-import { useFirebaseStore } from 'stores/firebase'
-const storeFirebase= useFirebaseStore();
-onBeforeMount(()=> {
-  storeFirebase.handleAuthStateChange();
-})
+  import { onBeforeMount, onMounted, ref } from 'vue';
+  import { useFirebaseStore } from 'stores/firebase'
+  const storeFirebase= useFirebaseStore();
+  const bar= ref(null)
+  onBeforeMount(()=> {
+    storeFirebase.handleAuthStateChange();    
+  })
+  onMounted(() => {
+    storeFirebase.setBar(bar.value);
+  })
+
 </script>
